1badf4bf8ba45d47422fe1c69e8999c031dcd478
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-12-08 04:19:01 +0000

    Feat: Update Alpine 3.19

--- speedtest_exporter.py
+++ speedtest_exporter.py
@@ -21,7 +21,7 @@ import pytz
 import requests
 from prometheus_client import PLATFORM_COLLECTOR, PROCESS_COLLECTOR
 from prometheus_client.core import REGISTRY, CollectorRegistry, Metric
-from prometheus_client.exposition import _bake_output, parse_qs
+from prometheus_client.exposition import _bake_output, _SilentHandler, parse_qs
 
 # Exporter Name Definition
 SPEEDTEST_EXPORTER_NAME = os.environ.get(
@@ -84,7 +84,7 @@ def start_wsgi_server(
 ) -> None:
     """Starts a WSGI server for prometheus metrics as a daemon thread."""
     app = make_wsgi_app(registry)
-    httpd = make_server(addr, port, app)
+    httpd = make_server(addr, port, app, handler_class=_SilentHandler)
     thread = threading.Thread(target=httpd.serve_forever)
     thread.daemon = True
     thread.start()
